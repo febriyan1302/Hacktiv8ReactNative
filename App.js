2cd5cfa9fd358729e9c15777921efc88c1cd8284
fajarfebriano <fajarfebriano@gmail.com>
2019-10-01 23:45:39 +0700

    Tugas Portofolio

--- App.js
+++ App.js
@@ -1,109 +1,197 @@
-// import React, { Component } from 'react';
-// import { Text, View, StyleSheet } from 'react-native';
-// import ImagesExample from './image_example.js';
-// import ImageDesc from './image_desc';
-// import Content from './content';
-// import Frisbee from './rest_api';
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, Image, ScrollView } from "react-native";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 
-// export default class HelloWorldApp extends Component{
-//   render(){
-//     return(
-//       <View>
-//         {/* <ImagesExample />
-//         <ImageDesc />
-//         <Content /> */}
+export default function App() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <ScrollView showsVerticalScrollIndicator={false}>
 
-//         <Text>Test Rest !</Text>
-//         <Text>{ Frisbee }</Text>
-//       </View>
-//     );
-//   }
-// }
+                <View style={{ alignSelf: "center", marginTop: 40}}>
+                    <View style={styles.profileImage}>
+                        <Image source={require("./assets/profile-pic.jpg")} style={styles.image} resizeMode="center"></Image>
+                    </View>
+                </View>
 
-// const styles = StyleSheet.create({
-//   baseText: {
-//     flex: 1, 
-//     justifyContent: "center", 
-//     alignItems:"center"
-//   },
-// })
+                <View style={styles.infoContainer}>
+                    <Text style={[styles.text, { fontWeight: "200", fontSize: 36 }]}>Fajar Febriano</Text>
+                    <Text style={[styles.text, { color: "#AEB5BC", fontSize: 14 }]}>Bukan Full Stack Developer</Text>
+                </View>
 
-// import React from 'react';
-// import { Button, Icon, ThemeProvider } from 'react-native-elements';
-// // import Icon from 'react-native-vector-icons/FontAwesome';
-// // import Icon from 'react-native-vector-icons/AntDesign';
-// // import Icon from "react-native-vector-icons/AntDesign";
+                <View style={styles.statsContainer}>
+                    <View style={styles.statsBox}>
+                        <Text style={[styles.text, { fontSize: 24 }]}>483</Text>
+                        <Text style={[styles.text, styles.subText]}>Posts</Text>
+                    </View>
+                    <View style={[styles.statsBox, { borderColor: "#DFD8C8", borderLeftWidth: 1, borderRightWidth: 1 }]}>
+                        <Text style={[styles.text, { fontSize: 24 }]}>45,844</Text>
+                        <Text style={[styles.text, styles.subText]}>Followers</Text>
+                    </View>
+                    <View style={styles.statsBox}>
+                        <Text style={[styles.text, { fontSize: 24 }]}>302</Text>
+                        <Text style={[styles.text, styles.subText]}>Following</Text>
+                    </View>
+                </View>
+                <Text style={[styles.subText, styles.recent]}>Skills</Text>
+                <View style={{ alignItems: "center" }}>
+                    <View style={styles.recentItem}>
+                        <View style={styles.activityIndicator}></View>
+                        <View style={{ width: 250 }}>
+                            <Text style={[styles.text, { color: "#41444B", fontWeight: "300" }]}>
+                                Python With Django Framework
+                            </Text>
+                        </View>
+                    </View>
 
-// const App = () => {
-//   return (
-//     <ThemeProvider>
-//       <Button 
-//         icon={
-//           <Icon
-//             name="aperture"
-//             size={25}
-//             color="white"
-//           />
-//         }
-//         title=" Home" 
-//       />
-//     </ThemeProvider>
-//   );
-// };
+                    <View style={styles.recentItem}>
+                        <View style={styles.activityIndicator}></View>
+                        <View style={{ width: 250 }}>
+                            <Text style={[styles.text, { color: "#41444B", fontWeight: "300" }]}>
+                                PHP With Laravel & Codeigniter Framework
+                            </Text>
+                        </View>
+                    </View>
 
-// export default App;
+                    <View style={styles.recentItem}>
+                        <View style={styles.activityIndicator}></View>
+                        <View style={{ width: 250 }}>
+                            <Text style={[styles.text, { color: "#41444B", fontWeight: "300" }]}>
+                                React JS & React Native
+                            </Text>
+                        </View>
+                    </View>
 
-// import React, {Component} from 'react';
-// import Budaya from './src/rest_budaya';
-
-// // type Props = {};
-// export default class App extends Component {
-//   render() {
-//     return (
-//         <Budaya />
-//     );
-//   }
-// }
-
-// ================= SWIPE GESTURE
-import React from 'react';
-import { StyleSheet, SafeAreaView, FlatList, Text, View, RectButton } from 'react-native';
-import ListItem, { Separator } from './src/listitem';
-import { colors } from 'react-native-elements';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
-
-export default class App extends React.Component{
-  render(){
-    return(
-      <SafeAreaView style={styles.container}>
-        <FlatList 
-          data={goods}
-          keyExtractor={item => item.id}
-          renderItem={({ item }) => (
-            <ListItem 
-              {...item}
-              onSwipeFromLeft={() => alert('swiped from left')}
-              OnRightPress={() => alert('pressed right')}
-            />
-            // <Text>{item.text}</Text>
-          )}
-          ItemSeparatorComponent={() => <Separator />}
-        />
-      </SafeAreaView>
+                    <View style={styles.recentItem}>
+                        <View style={styles.activityIndicator}></View>
+                        <View style={{ width: 250 }}>
+                            <Text style={[styles.text, { color: "#41444B", fontWeight: "300" }]}>
+                                And everything you need..
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
-  }
 }
 
-const goods = [
-  {"id": '0', "text": "Soap"},
-  {"id": '1', "text": "Shampo"},
-  {"id": '2', "text": "Conditioner"},
-  {"id": '3', "text": "Toothpaste"},
-  {"id": '4', "text": "Teeth Brush"}
-];
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#FFF"
+    },
+    text: {
+        color: "#52575D"
+    },
+    image: {
+        flex: 1,
+        height: undefined,
+        width: undefined
+    },
+    titleBar: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginTop: 24,
+        marginHorizontal: 16
+    },
+    subText: {
+        fontSize: 12,
+        color: "#AEB5BC",
+        textTransform: "uppercase",
+        fontWeight: "500"
+    },
+    profileImage: {
+        width: 200,
+        height: 200,
+        borderRadius: 100,
+        overflow: "hidden"
+    },
+    dm: {
+        backgroundColor: "#41444B",
+        position: "absolute",
+        top: 20,
+        width: 40,
+        height: 40,
+        borderRadius: 20,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    active: {
+        backgroundColor: "#34FFB9",
+        position: "absolute",
+        bottom: 28,
+        left: 10,
+        padding: 4,
+        height: 20,
+        width: 20,
+        borderRadius: 10
+    },
+    add: {
+        backgroundColor: "#41444B",
+        position: "absolute",
+        bottom: 0,
+        right: 0,
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    infoContainer: {
+        alignSelf: "center",
+        alignItems: "center",
+        marginTop: 16
+    },
+    statsContainer: {
+        flexDirection: "row",
+        alignSelf: "center",
+        marginTop: 32
+    },
+    statsBox: {
+        alignItems: "center",
+        flex: 1
+    },
+    mediaImageContainer: {
+        width: 180,
+        height: 200,
+        borderRadius: 12,
+        overflow: "hidden",
+        marginHorizontal: 10
+    },
+    mediaCount: {
+        backgroundColor: "#41444B",
+        position: "absolute",
+        top: "50%",
+        marginTop: -50,
+        marginLeft: 30,
+        width: 100,
+        height: 100,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 12,
+        shadowColor: "rgba(0, 0, 0, 0.38)",
+        shadowOffset: { width: 0, height: 10 },
+        shadowRadius: 20,
+        shadowOpacity: 1
+    },
+    recent: {
+        marginLeft: 78,
+        marginTop: 32,
+        marginBottom: 6,
+        fontSize: 10
+    },
+    recentItem: {
+        flexDirection: "row",
+        alignItems: "flex-start",
+        marginBottom: 16
+    },
+    activityIndicator: {
+        backgroundColor: "#CABFAB",
+        padding: 4,
+        height: 12,
+        width: 12,
+        borderRadius: 6,
+        marginTop: 3,
+        marginRight: 20
+    }
+});
